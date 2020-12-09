43574e413e92a905f905db17a74bc656ffd99bb9
JacquesCorentin <74654858+JacquesCorentin@users.noreply.github.com>
2020-12-09 12:41:53 +0100

    Update Exam.py

--- Exam.py
+++ Exam.py
@@ -2,6 +2,18 @@ from colorama import init
 init()
 from colorama import Force, Black, Style
 
+import random
+#mots inconnus
+
+mot1 = "castor"
+mot2 = "cinema"
+mot3 = "cypres"
+mot4 = "planer"
+mot5 = "retard"
+from colorama import init
+init()
+
+
 #mots inconnus
 
 mot1 = "castor"
@@ -27,13 +39,13 @@ tour = 0
 tableau_mystere = ["c","i","t","r","o","n"]
 tableau_joueur = ["_","_","_","_","_","_"]
 print(mot1, mot2, mot3, mot4, mot5, mot6, mot7, mot8, mot9,mot10)
-print(tableau)
-print("vous avez 8 tours pour trouver la solution, vous êtes au tour :",tour)
+print(tableau_joueur)
+print("vous avez 8 tours pour trouver la solution d'un mot de 6 lettres,vous êtes au tour :",tour)
 
 #Début de code
 def tableaujoueur(tableau_mystere, tableau_joueur):
     for i in range (1,7):
-        tableau_joueur[i-1] = input("inscrivez votre mot :")
+        tableau_joueur[i-1] = mot_joueur
     return tableau_joueur
 
 def choixdecaractere (tableau_mystere, tableau_joueur, verif):
@@ -43,44 +55,49 @@ def choixdecaractere (tableau_mystere, tableau_joueur, verif):
         if tableau_joueur[i-1] != tableau_mystere[1] and tableau_mystere[2] and tableau_mystere[3] and tableau_mystere[4] and tableau_mystere[5] and tableau_mystere[6]:
             verif = 1
         if verif == 2 : 
-        	tour += 1
-            print ("essaie encore, vous êtes au tour :", tour)
+        	tour = tour + 1
+        print ("essaie encore, vous êtes au tour :", tour)
     
 
-def gagner(tableau_mystere, tableau_joueur):
-	for i in range (1,7):
-		if tableau_joueur[i-1] == tableau_mystere[1] and tableau_mystere[2] and tableau_mystere[3] and tableau_mystere[4] and tableau_mystere[5] and tableau_mystere[6]:
-            verif = 3
-            print ("vous êtes au tour ",tour)
-			print ("Trouvé !!")
-	return
+#def gagner(tableau_mystere, tableau_joueur):
+#	for i in range (1,7):
+#		if tableau_joueur[i-1] == tableau_mystere[1] and tableau_mystere[2] and tableau_mystere[3] and tableau_mystere[4] and tableau_mystere[5] and tableau_mystere[6]:
+ #           verif = 3
+ #           print ("vous êtes au tour ",tour)
+#			print ("Trouvé !!")
+#	return
 	
 
-def retry(tableau_mystere, tableau_joueur):
-	for i in range (1,7):	
-		if tableau_joueur[i-1] == tableau_mystere[1] or tableau_mystere[2] or tableau_mystere[3] or tableau_mystere[4] or tableau_mystere[5] or tableau_mystere[6]:
-            verif = 1
-            tour += 1
-			print("essaie encore, vous êtes au tour :",tour)
-	return
+#def retry(tableau_mystere, tableau_joueur):
+#	for i in range (1,7):	
+#		if tableau_joueur[i-1] == tableau_mystere[1] or tableau_mystere[2] or tableau_mystere[3] or tableau_mystere[4] or tableau_mystere[5] or tableau_mystere[6]:
+ #           verif = 1
+ #           tour += 1
+#			print("essaie encore, vous êtes au tour :", tour)
+#	return
 
 
 def perdu(tableau_mystere, tableau_joueur):
 	for i in range (1,7):
 		if tour >= 8:
-			print("partie perdu, vous êtes arrivé au tour :",tour)
+		    print("partie perdu, vous êtes arrivé au tour :", tour)
 	return
 
+#def color (tableau_mystere ,tableau_joueur):
+#	while tableau_joueur[1] == tableau_mystere[1]:
+#		print(Force.RED + tableau_joueur[1])
 
-
-
+mot_joueur = input("inscrivez votre mot :")
+tableau_joueur = mot_joueur
 #Fin de code
+while tour < 8 :
+    tour = tour + 1
 #print(mot_joueur)
 deftableaujoueur (tableau_mystere, tableau_joueur)
-defperdu (tableau_mystere, tableau_joueur)
-defretry (tableau_mystere, tableau_joueur)
-defgagner (tableau_mystere, tableau_joueur)
-defchoixdecaractere (tableau_mystere, tableau_joueur)
-verif=3
+#defperdu (tableau_mystere, tableau_joueur)
+#defretry (tableau_mystere, tableau_joueur)
+#defgagner (tableau_mystere, tableau_joueur)
+#defchoixdecaractere (tableau_mystere, tableau_joueur)
+#verif=3
 print(tableau_joueur)
 input()
